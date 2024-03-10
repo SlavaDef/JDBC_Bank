33225cbad365813730ff8f90ef8c8fa55564eaa1
SlavaDef <slavadef@gmail.com>
2024-03-10 11:54:00 +0200

    First commit

---
+++ src/main/java/com/progItHomework/Main.java
@@ -0,0 +1,10 @@
+package com.progItHomework;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/com/progItHomework/entity/Scores.java
@@ -0,0 +1,4 @@
+package com.progItHomework.entity;
+
+public class Scores {
+}

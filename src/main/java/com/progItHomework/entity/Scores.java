321177551b8357686e59df08a926c9b3520871b6
SlavaDef <slavadef@gmail.com>
2024-03-10 21:28:33 +0200

    comit 5

--- src/main/java/com/progItHomework/entity/Scores.java
+++ src/main/java/com/progItHomework/entity/Scores.java
@@ -1,4 +1,19 @@
 package com.progItHomework.entity;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class Scores {
+
+    private String USD;
+
+    private String EUR;
+
+    private String UAN;
+
+
 }

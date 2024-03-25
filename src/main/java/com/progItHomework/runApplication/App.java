2338ef8a6a7f64cb4d78583a347e43eafc574bf4
SlavaDef <slavadef@gmail.com>
2024-03-25 23:00:32 +0200

    comit 20

--- src/main/java/com/progItHomework/runApplication/App.java
+++ src/main/java/com/progItHomework/runApplication/App.java
@@ -6,7 +6,7 @@ import static com.progItHomework.runApplication.Application.runApplication;
 public class App {
     public static void main(String[] args) {
 
-        runApplication();
+          runApplication();
 
     }
 }
